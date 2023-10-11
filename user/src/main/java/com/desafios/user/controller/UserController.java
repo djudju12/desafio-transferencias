0c640eaacfb63bee7d093677def8dbdd6acdcad8
jonathan <jonathan.willian321@gmail.com>
2023-10-11 20:19:36 -0300

    docker compose with grafana loki and tempo

--- user/src/main/java/com/desafios/user/controller/UserController.java
+++ user/src/main/java/com/desafios/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.desafios.user.controller;
 import com.desafios.user.model.UserDTO;
 import com.desafios.user.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
+@Slf4j
 public class UserController {
 
     private UserService service;
 
     @GetMapping(value = "/{userId}", produces = "application/json")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long userId) {
+        log.info("getUser: {}", userId);
         var user = service.getUser(userId);
         return ResponseEntity.ok(user);
     }
